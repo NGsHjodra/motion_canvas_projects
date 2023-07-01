e532db6e21ba7f66ee3e9c2e511cdfcbe0e1213a
Niyakit <niyakit123@gmail.com>
2023-07-02 01:08:37 +0700

    add new thunder

--- src/scenes/thunder.tsx
+++ src/scenes/thunder.tsx
@@ -3,20 +3,42 @@ import {Spline} from '@motion-canvas/2d/lib/components';
 
 export default makeScene2D(function* (view) {
     view.fill('#242424');
+    // view.add(
+    //     <>
+    //         <Spline
+    //             lineWidth={6}
+    //             stroke={'yellow'}
+    //             points={[
+    //             [-80, -380],
+    //             [240, -380],
+    //             [50, -100],
+    //             [240, -150],
+    //             [-220, 400],
+    //             [-80, 30],
+    //             [-240, 50],
+    //             [-80, -380],
+    //             ]}
+    //             smoothness={0}
+    //             // scale={0.5}
+    //             // rotation={-90}
+    //             // fill={'yellow'}
+    //         />
+    //     </>,
+    // );
+
     view.add(
         <>
             <Spline
                 lineWidth={6}
                 stroke={'yellow'}
                 points={[
-                [-80, -380],
-                [240, -380],
-                [50, -100],
-                [240, -150],
+                [20, -400],
+                [-80, -70],
+                [120, -120],
                 [-220, 400],
                 [-80, 30],
-                [-240, 50],
-                [-80, -380],
+                [-260, 70],
+                [20, -400],
                 ]}
                 smoothness={0}
                 // scale={0.5}
